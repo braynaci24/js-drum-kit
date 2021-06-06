bdc95e2e17480e0ca6a63ac8b097b32aae3852bf
braynaci24 <aynacimuhammetemin@gmail.com>
2021-06-06 16:59:42 +0300

    js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@ const removeTransition = e => {
     e.target.classList.remove('playing')
 }
 
-
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
